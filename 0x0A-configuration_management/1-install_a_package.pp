eedbe28799ee495c96c42321a96be51dbad9b545
AmenMengstu <99494058+AmenMengstu@users.noreply.github.com>
2022-09-19 11:47:38 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# install puppet flask
-package { 'puppet-flask':
+# Install the puppet-lint package
+package { 'puppet-lint':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'gem',
 }
