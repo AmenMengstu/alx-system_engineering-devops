2dafddc8dd8f73518f8e90c11019a9a4f7bd1b66
AmenMengstu <99494058+AmenMengstu@users.noreply.github.com>
2022-09-20 08:51:31 +0300

    Create 100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Set up ssh_config to reject password authentication
+
+file_line { 'password auth':
+	path  => '/etc/ssh/ssh_config',
+	match => '^ PasswordAuthentication',
+	line  => ' PasswordAuthentication no',
+}
+
+file_line { 'set identity file':
+	path  => '/etc/ssh/ssh_config',
+	match => '^ IdentityFile',
+	line  => ' IdentityFile ~/.ssh/holberton',
+}
